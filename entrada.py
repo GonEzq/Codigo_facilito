e19987f9a938063b3bd95faf1755f061261e87c9
GonEzq <gonzaloeriveral@gmail.com>
2023-07-02 10:16:14 -0300

    02-07

--- entrada.py
+++ entrada.py
@@ -1,2 +1,20 @@
 nombre_completo = input ('ingresa tu nombre completo: ') #str
 print (nombre_completo)
+print (type(nombre_completo))
+
+edad = input("ingresa tu edad: ")
+edad = int(edad)
+print(edad)
+print (type(edad))
+
+
+
+altura = input("ingresa tu altura: ")
+altura = float(altura)
+print(altura)
+print (type(altura))
+
+autorizacion = input (' ¿Autorizas el programa? (si/no)')
+autorizacion = autorizacion == 'si'
+
+print(autorizacion)
