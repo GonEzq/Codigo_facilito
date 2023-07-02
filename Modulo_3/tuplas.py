0602b136b021e5cbc0c6422bcb6773ad3c411f5f
GonEzq <gonzaloeriveral@gmail.com>
2023-07-02 16:19:44 -0300

    02-07

---
+++ Modulo_3/tuplas.py
@@ -0,0 +1,20 @@
+# el indice comienza en 0
+
+tupla = ('string', 10, 15.5, True, [1, 2, 3], (4,5,6))
+
+print (tupla)
+
+# --------------------------------------------------
+
+cursos = ('python', 'Flask', 'Django', 'Rails', 'MongoDB')
+
+primer_curso = cursos [0]
+
+print(primer_curso)
+
+ultimo_curso = cursos [-1]
+print(ultimo_curso)
+
+sub_tupla = cursos [:3]
+
+print(sub_tupla)
